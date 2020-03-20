b162ecc15b8ad2f8a65be56dcd2b94fc50312ab2
wangxin <wangxin.king@foxmail.com>
2020-03-20 16:47:44 +0800

    拒绝好友申请

--- app/App/HttpController/User.php
+++ app/App/HttpController/User.php
@@ -14,28 +14,34 @@ use App\Model\GroupMemberModel;
 use App\Model\GroupModel;
 use App\Model\SystemMessageModel;
 use App\Model\UserModel;
+use EasySwoole\ORM\DbManager;
 use EasySwoole\Pool\Manager;
 
 class User extends Base
 {
-    public function userInfo()
-    {
-        $token = $this->request()->getRequestParam('token');
+    protected $user;
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $token = $this->request()->getRequestParam('token');
         $redis = Manager::getInstance()->get('Redis')->getObj();
         $user  = $redis->get('User_token_' . $token);
         Manager::getInstance()->get('Redis')->recycleObj($redis);
 
-        if (!$user) {
+        $this->user = json_decode($user, true);
+        if (!$this->user) {
             return $this->writeJson(10001, '获取用户信息失败');
         }
+    }
 
-        $user = json_decode($user, true);
-
+    public function userInfo()
+    {
         // 我的群
         $groups = GroupMemberModel::create()->alias('gm')->join('`group` as g', 'gm.group_id = g.id')
             ->field('g.id,g.groupname,g.avatar')
-            ->where('gm.user_id', $user['id'])
+            ->where('gm.user_id', $this->user['id'])
             ->all();
         if ($groups) {
             foreach ($groups as $key => $group) {
@@ -44,14 +50,14 @@ class User extends Base
         }
 
         // 我的好友分组
-        $friendGroups = FriendGroupModel::create()->field('id,groupname')->where('user_id', $user['id'])->all();
+        $friendGroups = FriendGroupModel::create()->field('id,groupname')->where('user_id', $this->user['id'])->all();
         if ($friendGroups) {
             foreach ($friendGroups as $k => $v) {
                 // 小组里面的好友
                 $friendGroups[$k]['list'] = FriendModel::create()->alias('f')
                     ->field('u.nickname as username,u.id,u.avatar,u.sign,u.status')
                     ->join('user as u', 'u.id = f.user_id')
-                    ->where('f.user_id', $user['id'])
+                    ->where('f.user_id', $this->user['id'])
                     ->where('f.friend_group_id', $v['id'])
                     ->order('status', 'desc')
                     ->all();
@@ -60,11 +66,11 @@ class User extends Base
 
         $data = [
             'mine'   => [
-                'username' => $user['nickname'] . '(' . $user['id'] . ')',
-                'id'       => $user['id'],
-                'status'   => $user['status'],
-                'sign'     => $user['sign'],
-                'avatar'   => $user['avatar']
+                'username' => $this->user['nickname'] . '(' . $this->user['id'] . ')',
+                'id'       => $this->user['id'],
+                'status'   => $this->user['status'],
+                'sign'     => $this->user['sign'],
+                'avatar'   => $this->user['avatar']
             ],
             "friend" => $friendGroups,
             "group"  => $groups
@@ -114,25 +120,13 @@ class User extends Base
      */
     public function messageBox()
     {
-        $params = $this->request()->getRequestParam();
-        $token  = $params['token'];
-
-        $redis = Manager::getInstance()->get('Redis')->getObj();
-        $user  = $redis->get('User_token_' . $token);
-        Manager::getInstance()->get('Redis')->recycleObj($redis);
-
-        $user = json_decode($user, true);
-        if (!$user) {
-            return $this->writeJson(10001, '获取用户信息失败');
-        }
-
         // 系统消息标记已读
-        SystemMessageModel::create()->where('user_id', $user['id'])->update(['read' => 1]);
+        SystemMessageModel::create()->where('user_id', $this->user['id'])->update(['read' => 1]);
 
         $list = SystemMessageModel::create()->alias('sm')
             ->join('user as u', 'sm.user_id = u.id')
             ->field('sm.id,u.id as uid,u.avatar,u.nickname,sm.remark,sm.time,sm.type,sm.group_id,sm.status')
-            ->where('user_id', $user['id'])
+            ->where('user_id', $this->user['id'])
             ->order('sm.id', 'desc')
             ->limit(50)
             ->all();
@@ -187,20 +181,15 @@ class User extends Base
     public function addFriend()
     {
         $params          = $this->request()->getRequestParam();
-        $token           = $params['token'];
         $systemMessageId = $params['id'];
-
-        $redis = Manager::getInstance()->get('Redis')->getObj();
-        $user  = $redis->get('User_token_' . $token);
-        Manager::getInstance()->get('Redis')->recycleObj($redis);
-
-        $user = json_decode($user, true);
-        if (!$user) {
-            return $this->writeJson(10001, '获取用户信息失败');
-        }
+        $groupId         = $params['group_id'];
 
         $systemMessage = SystemMessageModel::create()->where('id', $systemMessageId)->get();
-        $isFriend      = FriendModel::create()->where('user_id', $user['id'])->where('friend_id', $systemMessage['user_id'])->get();
+        if (!$systemMessage) {
+            // TODO 错误处理
+        }
+
+        $isFriend = FriendModel::create()->where('user_id', $this->user['id'])->where('friend_id', $systemMessage['user_id'])->get();
         if ($isFriend) {
             return $this->writeJson(10001, '已经是好友了');
         }
@@ -210,7 +199,7 @@ class User extends Base
             [
                 'user_id'         => $systemMessage['user_id'],
                 'friend_id'       => $systemMessage['from_id'],
-                'friend_group_id' => $params['group_id'],
+                'friend_group_id' => $groupId,
             ],
             [
                 'user_id'         => $systemMessage['from_id'],
@@ -238,12 +227,58 @@ class User extends Base
             'avatar'   => $friendInfo['avatar'],
             'username' => $friendInfo['username'],
             'groupid'  => $params['group_id'],
-            'id'       => $user['id'],
+            'id'       => $this->user['id'],
             'sign'     => $friendInfo['sign'],
         ];
 
         return $this->writeJson(200, '添加成功', $data);
     }
 
+    /**
+     * 拒绝好友申请
+     *
+     * @return bool
+     * @throws \EasySwoole\ORM\Exception\Exception
+     * @throws \Throwable
+     * @throws \EasySwoole\Mysqli\Exception\Exception
+     * @author King
+     */
+    public function refuseFriend()
+    {
+        $systemMessageId = $this->request()->getRequestParam('id');
 
+        $systemMessage = SystemMessageModel::create()->where('id', $systemMessageId)->get();
+        if (!$systemMessage) {
+            // TODO 错误处理
+        }
+
+        // 开启事务
+        DbManager::getInstance()->startTransaction();
+
+        // 标记已拒绝
+        $res = SystemMessageModel::create()->where('id', $systemMessageId)->update(['status' => 2]);
+
+        // 给申请添加这发送一条拒绝的系统消息
+        $systemMessageData = [
+            'user_id' => $systemMessage['from_id'],
+            'from_id' => $systemMessage['user_id'],
+            'type'    => 1,
+            'status'  => 2,
+            'time'    => time()
+        ];
+        $result            = SystemMessageModel::create()->data($systemMessageData)->save();
+
+        if ($res && $result) {
+
+            // 提交事务
+            DbManager::getInstance()->commit();
+
+            return $this->writeJson(200, '已拒绝');
+        }
+
+        // 回滚事务
+        DbManager::getInstance()->rollback();
+
+        return $this->writeJson(10001, '操作失败');
+    }
 }
